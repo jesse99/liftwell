8ab4ca60132645834e0a0881332e74922accdb6a
Jesse Jones <jesse9jones@gmail.com>
2018-12-02 19:51:27 -0800

    Better handle unusual case where percents are greater than 100

--- liftwell/SubTypes/PercentSubType.swift
+++ liftwell/SubTypes/PercentSubType.swift
@@ -90,7 +90,13 @@ class PercentSubType: BaseRepsApparatusSubType {
     }
     
     override func worksetBias() -> Int {
-        return -1
+        if percent < 1.0 {
+            return -1
+        } else if percent > 1.0 {
+            return 1
+        } else {
+            return 0
+        }
     }
     
     private func otherWeight() -> Double? {
