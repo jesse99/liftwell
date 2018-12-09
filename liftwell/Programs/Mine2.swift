e6b125de59a2817d4ac7f0dfbbf28a6110d29cc9
Jesse Jones <jesse9jones@gmail.com>
2018-12-08 18:26:36 -0800

    Added a nifty limber 11 link

--- liftwell/Programs/Mine2.swift
+++ liftwell/Programs/Mine2.swift
@@ -7,11 +7,11 @@ func Mine2() -> Program {
         // Heavy
         dumbbell2("Heavy Bench",    "Dumbbell Bench Press",            "10@50% 3@70% 1@90%", "6-12".x(3, rest: .atEnd), restMins: 4.0, main: true),
         dumbbell2("Heavy Squat",    "Dumbbell Single Leg Split Squat", "5@50% 3@75%",        "4-8".x(3, rest: .atEnd), restMins: 4.0, main: true),
-        dumbbell1("Kroc Row",       "Kroc Row",                        "5@50% 3@75%",        "5-10".x(3, rest: .atEnd), restMins: 3.0, main: true),
+        dumbbell1("Kroc Row",       "Kroc Row",                        "5@50% 3@75%",        "4-10".x(3, rest: .atEnd), restMins: 3.0, main: true),
         dumbbell2("Dumbbell Flyes", "Dumbbell Flyes",                  "10@50% ",            "6-12".x(3), restMins: 3.0),
 
         // Light
-        dumbbell2("Dumbbell OHP",    "Dumbbell Shoulder Press", "10@50% 3@70% 1@90%", "5-10".x(3, rest: .atEnd), restMins: 3.5, main: true),
+        dumbbell2("Dumbbell OHP",    "Dumbbell Shoulder Press", "10@50% 3@70% 1@90%", "3-8".x(3, rest: .atEnd), restMins: 3.5, main: true),
         dumbbell1("Back Extensions", "Back Extensions",         "10@50%",             "6-12".x(3), restMins: 3.0, main: true),
         bodyweight("Chinups",        "Chinup", numSets: 4, goalReps: 25, restMins: 3.5, restAtEnd: true, main: true),
         dumbbell2("Farmer's Walk",   "Farmer's Walk",           "",                   "1".x(2), restMins: 3.0),
@@ -30,8 +30,7 @@ func Mine2() -> Program {
         bodyweight("Fire Hydrant Hip Circle", "Fire Hydrant Hip Circle",   1, by: 10),
         bodyweight("Mountain Climber",        "Mountain Climber",          1, by: 10),
         bodyweight("Cossack Squat",           "Cossack Squat",             1, by: 10),
-        bodyweight("Piriformis Stretch",      "Seated Piriformis Stretch", 2, secs: 30),
-        bodyweight("Hip Flexor Stretch",      "Rear-foot-elevated Hip Flexor Stretch", 2, secs: 30)
+        bodyweight("Piriformis Stretch",      "Seated Piriformis Stretch", 2, secs: 30)
     ]
     
     let workouts = [
@@ -43,8 +42,9 @@ func Mine2() -> Program {
     
     let tags: [Program.Tags] = [.intermediate, .strength, .dumbbell, .unisex, .threeDays, .age40s, .age50s]
     
+    // TODO: add a barbell masters program and change the description to recommend that
     let description = """
-This is one of the programs I use when I don't have access to a gym with barbells. It's a three day a week program and the days look like this:
+This is a dumbbell only program for older lifters. It's based on programming principles from the book [The Barbell Prescription: Strength Training for Life After 40](https://www.amazon.com/dp/B06Y4LXFCK/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1). The [Limber 11](https://imgur.com/gallery/iEsaS) stretches should ideally be done several times a week. It's a three day a week program and the days look like this:
 
 **Heavy** (should be a struggle to finish all the reps)
 * Dumbbell Bench 3x6-12
@@ -59,9 +59,20 @@ This is one of the programs I use when I don't have access to a gym with barbell
 
 **Medium**  (should feel like you are working hard but in no danger of missing a rep)
 * Dumbbell Bench 3x10 @ 94%
-* Bulgarian Split Squat 3x8 @ 94%
+* Bulgarian Split Squat 3x6 @ 94%
 * Chinups to 25 reps
 * Kroc Row 3x6-12
+
+**Limber 11 (these are optional)**
+* Foam Rolling
+* Shoulder Dislocates
+* Bent-knee Iron Cross
+* Roll-over into V-sit
+* Rocking Frog Stretch
+* Fire Hydrant Hip Circle
+* Mountain Climber
+* Cossack Squat
+* Piriformis Stretch
 """
     return Program("Mine2", workouts, exercises, tags, description)
 }
