14d9d7d332cccf4ad997b6d83d59c4bde7abb20e
Jesse Jones <jesse9jones@gmail.com>
2019-01-06 16:24:47 -0800

    Tweaked my program

--- liftwell/Programs/MastersDB.swift
+++ liftwell/Programs/MastersDB.swift
@@ -5,22 +5,23 @@ import Foundation
 func MastersDB() -> Program {
     let exercises = [
         // Heavy
-        dumbbell2("Heavy Bench",    "Dumbbell Bench Press",            "10@50% 3@70% 1@90%", "6-12".x(3, rest: .atEnd), restMins: 4.0, main: true),
-        dumbbell2("Heavy Squat",    "Dumbbell Single Leg Split Squat", "5@50% 3@75%",        "4-8".x(3, rest: .atEnd), restMins: 4.0, main: true),
-        dumbbell1("Kroc Row",       "Kroc Row",                        "10@50%",             "6-12".x(3, rest: .atEnd), restMins: 3.0, main: true),
-        dumbbell2("Dumbbell Flyes", "Dumbbell Flyes",                  "10@50% ",            "6-12".x(3), restMins: 3.0),
+        dumbbell2("Heavy Bench",     "Dumbbell Bench Press",            "10@50% 3@70% 1@90%", "6-12".x(3, rest: .atEnd), restMins: 4.0, main: true),
+        dumbbell2("Heavy Squat",     "Dumbbell Single Leg Split Squat", "5@50% 3@75%",        "4-8".x(3, rest: .atEnd), restMins: 4.0, main: true),
+        bodyweight("Chinups",        "Chinup",                          numSets: 3, startReps: 5, goalReps: 24, restMins: 3.5, restAtEnd: true, main: true),
+        dumbbell1("Back Extensions", "Back Extensions",                 "10@50%",             "6-12".x(3, rest: .atEnd), restMins: 3.0, main: true),
+        dumbbell1("Kroc Row",        "Kroc Row",                        "10@50%",             "6-12".x(3, rest: .atEnd), restMins: 3.0, main: true),
 
         // Light
+        dumbbell1("Deadlift",        "Dumbbell Deadlift",       "10@50%",             "6-12".x(3, rest: .atEnd), restMins: 4.0, main: true),
         dumbbell2("Dumbbell OHP",    "Dumbbell Shoulder Press", "10@50% 3@70% 1@90%", "3-8".x(3, rest: .atEnd), restMins: 3.5, main: true),
-        dumbbell1("Back Extensions", "Back Extensions",         "10@50%",             "6-12".x(3, rest: .atEnd), restMins: 3.0, main: true),
-        bodyweight("Chinups",        "Chinup", numSets: 4, startReps: 10, goalReps: 25, restMins: 3.5, restAtEnd: true, main: true),
+        dumbbell2("Dumbbell Flyes",  "Dumbbell Flyes",          "10@50% ",            "6-12".x(3), restMins: 3.0),
         dumbbell2("Farmer's Walk",   "Farmer's Walk",           "",                   "1".x(2), restMins: 3.0),
         cable("Cable Crunches",      "Cable Crunch",            "",                   "6-12".x(3), restMins: 3.0),
 
         // Medium
         dumbbell2("Medium Bench",    "Dumbbell Bench Press",            "10@50% 3@70% 1@90%", "4-10".x(2, rest: .atEnd), percent1RM: 0.94, other: "Heavy Bench", restMins: 4.0),
         dumbbell2("Medium Squat",    "Dumbbell Single Leg Split Squat", "5@50% 3@75%",        "4-8".x(2, rest: .atEnd), percent1RM: 0.94, other: "Heavy Squat", restMins: 4.0),
-        bodyweight("Dips",           "Dips", numSets: 3, startReps: 10, goalReps: 36, restMins: 3.5, restAtEnd: false),
+        bodyweight("Dips",           "Dips",                            numSets: 3, startReps: 10, goalReps: 36, restMins: 3.5, restAtEnd: false),
         
         bodyweight("Foam Rolling",            "IT-Band Foam Roll",         1, by: 15),
         bodyweight("Shoulder Dislocates",     "Shoulder Dislocate",        1, by: 12),
@@ -35,7 +36,7 @@ func MastersDB() -> Program {
     
     let workouts = [
         Workout("Heavy", ["Heavy Bench", "Heavy Squat", "Chinups", "Back Extensions", "Kroc Row"], scheduled: true, optional: ["Back Extensions"]),
-        Workout("Light", ["Dumbbell OHP", "Back Extensions", "Dumbbell Flyes", "Farmer's Walk", "Cable Crunches"], scheduled: true, optional: ["Farmer's Walk", "Cable Crunches"]),
+        Workout("Light", ["Deadlift", "Dumbbell OHP", "Back Extensions", "Dumbbell Flyes", "Farmer's Walk", "Cable Crunches"], scheduled: true, optional: ["Farmer's Walk", "Cable Crunches"]),
         Workout("Medium", ["Medium Bench", "Medium Squat", "Chinups", "Kroc Row", "Farmer's Walk", "Dips"], scheduled: true, optional: ["Dips", "Farmer's Walk"]),
         
         Workout("Mobility", ["Foam Rolling", "Shoulder Dislocates", "Bent-knee Iron Cross", "Roll-over into V-sit", "Rocking Frog Stretch", "Fire Hydrant Hip Circle", "Mountain Climber", "Cossack Squat", "Piriformis Stretch", "Hip Flexor Stretch"], scheduled: false)]
@@ -53,6 +54,7 @@ This is a dumbbell only program for older lifters. It's based on programming pri
 * Kroc Row 3x6-12
 
 **Light**
+* Deadlift 3x6-12
 * Dumbbell OHP 3x5-10
 * Back Extensions 3x6-12
 * Dumbbell Flyes 3x6-12
